036808c79d71304c34a64e8d8abfe58ce935c9bf
Lachezar Todorov <lachezar.todorov@edentechlabs.io>
2022-01-17 17:03:49 +0200

    Forwarded all delegate methods to the original observer

--- Sources/Focuser/TextFieldIntrospect.swift
+++ Sources/Focuser/TextFieldIntrospect.swift
@@ -9,12 +9,47 @@ import SwiftUI
 import Introspect
 
 class TextFieldObserver: NSObject, UITextFieldDelegate {
+    var originalObserver: UITextFieldDelegate?
     var onReturnTap: () -> () = {}
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        originalObserver?.textFieldShouldReturn?(textField)
         onReturnTap()
         return false
     }
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        return originalObserver?.textFieldShouldBeginEditing?(textField) ?? true
+    }
+
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        originalObserver?.textFieldDidBeginEditing?(textField)
+    }
+
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        return originalObserver?.textFieldShouldEndEditing?(textField) ?? true
+    }
+
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        originalObserver?.textFieldDidEndEditing?(textField)
+    }
+
+    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
+        originalObserver?.textFieldDidEndEditing?(textField, reason: reason)
+    }
+
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        return originalObserver?.textField?(textField, shouldChangeCharactersIn: range, replacementString: string) ?? true
+    }
+
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        originalObserver?.textFieldDidChangeSelection?(textField)
+    }
+
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        return originalObserver?.textFieldShouldClear?(textField) ?? true
+    }
+
 }
 
 public struct FocusModifier<Value: FocusStateCompliant & Hashable>: ViewModifier {
@@ -25,6 +60,7 @@ public struct FocusModifier<Value: FocusStateCompliant & Hashable>: ViewModifier
     public func body(content: Content) -> some View {
         content
             .introspectTextField { tf in
+                observer.originalObserver = tf.delegate
                 tf.delegate = observer
                 
                 /// when user taps return we navigate to next responder
